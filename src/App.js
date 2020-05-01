58ffe28e7494e5cef5638f5065679b0753496eee
Tyler Davis <datadazer@gmail.com>
2020-04-30 21:42:55 -0700

    Add konva, start cell and grid components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Stage, Layer, Text } from 'react-konva';
+import Cell from './components/cell/cell.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Cell />
+        </Layer>
+      </Stage>
     </div>
   );
 }
---
+++ src/components/grid/grid.component.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Rect } from 'react-konva';
+import Konva from 'konva';
+
+class Grid extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Grid;
